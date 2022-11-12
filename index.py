e99fa104d87ea51e497870ace56cf54a2ced4049
Puffy <88508824+PufflyMan@users.noreply.github.com>
2022-11-12 08:31:05 +0400

    first commit

---
+++ index.py
@@ -0,0 +1,17 @@
+import sys
+sys.stdin = open('input.txt', "r")
+
+t = int(input())
+
+while t>0:
+    l,n = input().split()
+    d,c = input().split()
+    unit = 0
+    for i in range(n):
+        
+
+
+# for _ in input():
+#     x = input()
+#     print(x)
+
